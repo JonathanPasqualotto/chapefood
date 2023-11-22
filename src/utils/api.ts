81b034f64aa42f82c91710554f794bc295a95fe2
Lunelli2k <gabriellunelli2k19@gmail.com>
2023-11-22 19:54:01 -0300

    api - troca do link

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/',        //SERVER RENDER
+    baseURL: 'https://chapefood-api.onrender.com',        //SERVER RENDER
 })
 
 export default api
